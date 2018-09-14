13555a584750dad742529a5c8117abb57f69ecef
Bo Chen <bo.chen@citrix.com>
2018-09-14 14:21:24 +0800

    support ES integration and tested

--- src/main/java/com/kristijangeorgiev/auth/TestContoller.java
+++ src/main/java/com/kristijangeorgiev/auth/TestContoller.java
@@ -1,5 +1,12 @@
 package com.kristijangeorgiev.auth;
 
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class TestContoller {
 
+	@Autowired
+	private ElasticsearchTemplate elasticsearchTemplate;
+	
 	@GetMapping("/searcher/")
 	public String hello() {
-		return "ddfadfasf";
+		SearchQuery sq = new NativeSearchQueryBuilder().withIndices("cdftrace").withQuery(QueryBuilders.queryStringQuery("error")).build();		
+		System.out.println("query ES:" + elasticsearchTemplate.count(sq));
+		return "searcher";
+	}
+
+	@GetMapping("/index-configger/")
+	public String hello2() {	
+		return "index-configger";
+	}
+	
+	@GetMapping("/searcher/hello")
+	public String hello3() {
+		return SecurityContextHolder.getContext().getAuthentication().getName();
 	}
 	
 }
